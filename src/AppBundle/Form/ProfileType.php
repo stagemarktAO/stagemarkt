17377d4c233424910316d49ef6084f5696d7d83f
Roan van Adrichem <roanvanadrichem98@live.nl>
2017-06-09 11:30:21 +0200

    choices update

--- src/AppBundle/Form/ProfileType.php
+++ src/AppBundle/Form/ProfileType.php
@@ -2,13 +2,31 @@
 
 namespace AppBundle\Form;
 
+
+use FOS\UserBundle\Util\LegacyFormHelper;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 class ProfileType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
+		$constraintsOptions = array(
+			'message' => 'Wachtwoord incorrect',
+		);
+
+		if (!empty($options['validation_groups'])) {
+			$constraintsOptions['groups'] = array(reset($options['validation_groups']));
+		}
+
+		$builder->add('current_password', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'), array(
+			'label' => 'Huidige wachtwoord',
+			'translation_domain' => 'FOSUserBundle',
+			'mapped' => false,
+			'constraints' => new UserPassword($constraintsOptions),
+		));
 		$builder->add('phoneNumber');
 		$builder->add('name');
 	}
--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -4,12 +4,18 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class RegistrationType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
-        $builder->add('loginType');
+        $builder->add('loginType', ChoiceType::class, array(
+        	'choices' => [
+        		'Bedrijf' => 0,
+		        'Student' => 1
+	        ]
+        ));
         $builder->add('name');
         $builder->add('phoneNumber');
     }
